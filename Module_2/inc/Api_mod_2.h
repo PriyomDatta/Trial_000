ba7e3fcb18ac040cbc91b86c56c79c9b5d0a8250
PriyomDatta <priyomdat@gmail.com>
2025-04-25 08:16:28 +0530

    First commit with class structure only

---
+++ Module_2/inc/Api_mod_2.h
@@ -0,0 +1,19 @@
+#ifndef MOD_2
+#define MOD_2
+
+
+class MOD_2{
+
+private:
+    char var_1;
+    int var_2;
+    void update();
+protected:
+public:
+    MOD_2(); 
+    void get_data();
+   
+};
+    
+    
+#endif
